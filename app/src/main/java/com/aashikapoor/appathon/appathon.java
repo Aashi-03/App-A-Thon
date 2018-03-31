66d38b5e323c67879834c56bb46e58e451e5b724
Aashi-03 <37948563+Aashi-03@users.noreply.github.com>
2018-03-31 11:26:32 +0530

    Mars Den

---
+++ app/src/main/java/com/aashikapoor/appathon/appathon.java
@@ -0,0 +1,13 @@
+package com.aashikapoor.appathon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class appathon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appathon);
+    }
+}
